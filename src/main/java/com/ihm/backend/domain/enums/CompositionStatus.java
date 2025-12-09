bce31cb097b2ffd58393da0d02e08ca60a3757bc
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2025-12-09 12:04:42 +0100

    version 1.00

--- src/main/java/com/ihm/backend/domain/enums/CompositionStatus.java
+++
@@ -1,7 +0,0 @@
-package cm.enspy.xccm.domain.enums;
-
-public enum CompositionStatus {
-    DRAFT, // Brouillon
-    PUBLISHED, // Publié
-    ARCHIVED // Archivé
-}
--- src/main/java/com/ihm/backend/domain/enums/GranuleType.java
+++
@@ -1,9 +0,0 @@
-
-package cm.enspy.xccm.domain.enums;
-
-public enum GranuleType {
-    TEXT, // Texte simple
-    IMAGE, // Image
-    PDF, // Document PDF
-    LINK // Lien externe
-}
